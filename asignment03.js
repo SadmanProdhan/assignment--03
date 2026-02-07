8b78eedd59e51b2af9403639fdcaf922d4e31b42
SadmanProdhan <sadman.lab.medhabi@gmail.com>
2026-02-07 07:43:45 +0600

    Update asignment03.js

--- asignment03.js
+++ asignment03.js
@@ -11,3 +11,31 @@ function newPrice(currentPrice, discount) {
     let finalPrice = currentPrice - discount*currentPrice/100;
     return finalPrice.toFixed(3);
 }
+
+//problem-02----------------------------------------------------------------//
+function validOtp(otp) {
+    // Your code here
+
+    if(typeof(otp) !== "string" ){
+        return "Invalid";
+    }
+    else if(otp.length !== 8 || !otp.startsWith("ph-") ){
+        return false;
+    }
+    return true;
+}
+//problem-03-------------------------------------//
+
+function finalScore(omr) {
+    //write your code here
+
+  
+
+    if(typeof omr !== "object" || omr.right + omr.wrong + omr.skip !==100 ){
+        return "Invalid";
+    }
+    else{let totalScore = omr.right - omr.wrong*0.5;
+    return Math.round(totalScore)}
+}
+
+//
